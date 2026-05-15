ba7ad993e62844c92243aea7945a9279303f14f6
nvavinash <avinashmk42@gmail.com>
2026-05-15 15:22:54 +0530

    cors added

--- server.js
+++ server.js
@@ -60,3 +60,8 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Running on ${PORT}`);
 });
+
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());

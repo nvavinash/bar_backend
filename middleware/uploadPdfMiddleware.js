158cc513fd9eef9b18c37cc0b1c60f391ca6563a
nvavinash <avinashmk42@gmail.com>
2026-05-18 17:52:03 +0530

    member fixed

--- middleware/uploadPdfMiddleware.js
+++ middleware/uploadPdfMiddleware.js
@@ -3,29 +3,17 @@ const { CloudinaryStorage } = require("multer-storage-cloudinary");
 const cloudinary = require("../config/cloudinary");
 
 // Cloudinary storage for PDF uploads (events / notices)
-// const pdfStorage = new CloudinaryStorage({
-//   cloudinary,
-//   params: {
-//     folder: "bar_uploads/pdfs",
-//     allowed_formats: ["pdf"],
-//     resource_type: "raw", // required for non-image files
-//     public_id: (req, file) =>
-//       `pdf_${Date.now()}_${Math.round(Math.random() * 1e9)}.pdf`,
-//   },
-// });
-
 const pdfStorage = new CloudinaryStorage({
   cloudinary,
   params: {
     folder: "bar_uploads/pdfs",
-
     allowed_formats: ["pdf"],
-
-    resource_type: "auto",
-
-    public_id: `pdf_${Date.now()}_${Math.round(Math.random() * 1e9)}`,
+    resource_type: "raw", // required for non-image files
+    public_id: (req, file) =>
+      `pdf_${Date.now()}_${Math.round(Math.random() * 1e9)}.pdf`,
   },
 });
+
 
 // PDF-only filter (unchanged)
 const pdfFilter = (req, file, cb) => {
